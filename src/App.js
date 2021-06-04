dedd378df427b7219462066691db9754db43d34c
IbraShaheen <engibraheem1995@gmail.com>
2021-06-04 17:43:01 +0300

    task2 | Product List

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import "./App.css" ;
+import products from "./products";
+
+const dataArray = products.map(product =>
+        
+<div>
+        <img className="juiceItem" src={product.pic} alt="menu"/>
+        <h4>{product.name}</h4>
+        <p>{product.price} KD</p>  
+</div>
+  )
 
 function App() {
-  
+
   return (
    <div className="centered-item">
       
@@ -9,7 +19,13 @@ function App() {
 
     <h4>Be healthy and enjoy your juice</h4>
 
-    <img src="1st.jpg" className="head-image"/>
+    <img
+    src="1st.jpg" alt="juiceShop" 
+    className="head-image"/>
+
+      <div className="productsParent">
+      {dataArray}
+      </div>
 
    </div>
   );
---
+++ src/products.js
@@ -0,0 +1,20 @@
+
+
+const products = [
+{
+    pic:"freshLemonade.jpg",
+    name: "Fresh Lemonade",
+    price: 2
+
+},
+{
+    pic:"blueberry.jpg",
+    name: "Blueberry",
+    price: 3.5
+
+}
+
+
+];
+
+export default products;
